3c9fcc583dadb70502dafafb2c4ae9fce9c7a002
danyocum <danyocum@gmail.com>
2020-06-01 09:30:45 -0700

    updated README for testing groups and targets, added hooks for (empty) clear-results script

--- README.md
+++ README.md
@@ -72,7 +72,13 @@ The solution for large datasets is to subdivide them into reasonably sized chunk
 
 ### Running unit tests
 
-When generating geoprocessing function templates using npm scripts, a test file will also be created (e.g. `functions/myFunction.test.ts`). These unit tests can be run using `npm test`. This template by default will run your function against all sketches in `examples/sketches` and save the output to `examples/output`, which will be used when debugging the geoprocessing clients.
+When generating geoprocessing function templates using npm scripts, two test files will also be created (e.g. `functions/myFunctionSmoke.test.ts` and `functions/myFunctionUnit.test.ts` ). The smoke tests have 2 objectives: make sure your function exists and write out the results of the runs with the example sketches. The unit tests execute the functions and can be used to test correct output values. All unit tests can be run using `npm test`. This template by default will run your function against all sketches in `examples/sketches` and save the output to `examples/output`, which will be used when debugging the geoprocessing clients. 
+
+Be default, each smoke test will be added to the @smoke group, and unit tests will be added to the @unit group. (Groups are specified in the docstring at the top of the file). To pass flags from npm to jest during tests, separate the npm test command from the jest flags using '--'. For example, to run just the smoke or unit test groups, you can send the --group flag to jest:
+npm test -- --group=smoke 
+
+You can also run a target test by using the -t option, for example, running just a test called 'areaTest':
+npm test -- -t=areaTest
 
 #### More resources
 
--- packages/geoprocessing/scripts/geoprocessing.ts
+++ packages/geoprocessing/scripts/geoprocessing.ts
@@ -9,6 +9,7 @@ if (process.argv.length < 3) {
   throw new Error("Missing command");
 } else {
   const command = process.argv[2];
+  console.log("geoprocessing....");
   switch (command) {
     case "test":
       runTests();
@@ -19,49 +20,49 @@ if (process.argv.length < 3) {
     case "create:function":
       spawn("node", [`${__dirname}/init/createFunction.js`], {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "create:client":
       spawn("node", [`${__dirname}/init/createClient.js`], {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "build:lambda":
       spawn(`${__dirname}/../../scripts/build/build.sh`, {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "build:client":
       spawn(`${__dirname}/../../scripts/build/build-client.sh`, {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "deploy":
       spawn(`${__dirname}/../../scripts/deploy/deploy.sh`, {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "bootstrap":
       spawn(`${__dirname}/../../scripts/deploy/bootstrap.sh`, {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "storybook":
       spawn(`${__dirname}/../../scripts/storybook.sh`, {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "url":
       spawn(`${__dirname}/../../scripts/deploy/url.sh`, {
         cwd: process.cwd(),
-        stdio: "inherit"
+        stdio: "inherit",
       });
       break;
     case "bundle-features":
@@ -70,9 +71,15 @@ if (process.argv.length < 3) {
         [`${__dirname}/dataPrep/bin.js`, ...process.argv.slice(2)],
         {
           cwd: process.cwd(),
-          stdio: "inherit"
+          stdio: "inherit",
         }
       );
+    case "clear-results":
+      console.log("clearing results...");
+      spawn(`${__dirname}/../../scripts/clear-results.sh`, {
+        cwd: process.cwd(),
+        stdio: "inherit",
+      });
       break;
     default:
       throw new Error(`Command ${command} not supported.`);

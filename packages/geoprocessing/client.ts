88cecf47a4cd4ba16ca9cc0083edcf1430d30793
danyocum <danyocum@gmail.com>
2020-08-06 09:39:56 -0700

    added intersect helper to client

--- packages/geoprocessing/client.ts
+++ packages/geoprocessing/client.ts
@@ -4,10 +4,9 @@ export { useFunction } from "./src/hooks/useFunction";
 export { default as SketchAttributesCard } from "./src/components/SketchAttributesCard";
 export { default as ReportDecorator } from "./src/components/ReportDecorator";
 export { default as Skeleton } from "./src/components/Skeleton";
-export { default as ReportTable } from "./src/components/ReportTable";
-
 export {
   getSketchIntersectList,
   getIntersectList,
   intersect,
 } from "./src/spatial/IntersectHelper";
+export { default as ReportTable } from "./src/components/ReportTable";

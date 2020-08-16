e4e7445364ef160b2aaed68d5f25a2761dda0f01
Chad Burt <chad@underbluewaters.net>
2020-08-16 14:37:37 -0700

    Use color rather than opacity to style Card headings, and allow titles to be ReactNodes or strings

--- packages/geoprocessing/src/components/Card.tsx
+++ packages/geoprocessing/src/components/Card.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React from "react";
 
 export interface Props {
-  title?: string;
-  children: React.ReactNode,
-  style?: object
+  title?: string | React.ReactNode;
+  children: React.ReactNode;
+  style?: object;
 }
 
 const boxStyle = {
-  fontFamily: 'sans-serif',
+  fontFamily: "sans-serif",
   borderRadius: 4,
   backgroundColor: "#fff",
-  boxShadow: "0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)",
+  boxShadow:
+    "0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)",
   padding: 16,
-  margin: "8px 0px"
-}
+  margin: "8px 0px",
+};
 
 const titleStyle = {
   fontSize: 14,
-  color: "rgba(0, 0, 0, 0.54)",
+  color: "rgba(116, 116, 116, 1)",
   marginBottom: "0.35em",
   marginTop: 0,
   fontWeight: 400,
-}
+};
 
 const Card = ({ children, title, style }: Props) => {
-  return <div style={{...boxStyle, ...(style || {})}}>
-    {title && title.length && <h2 style={titleStyle}>{title}</h2>}
-    {children}
-  </div>
-}
+  return (
+    <div style={{ ...boxStyle, ...(style || {}) }}>
+      {title && title !== "" && <h2 style={titleStyle}>{title}</h2>}
+      {children}
+    </div>
+  );
+};
 
 export default Card;

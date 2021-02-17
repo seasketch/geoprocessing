c1e0e6adc0acbfc48e6b1f79e3a6273a87c845b6
Chad Burt <chad@underbluewaters.net>
2021-02-17 08:47:34 -0800

    Adding css-loader and file-loader to webpack configuration so that clients can embed css and images

--- packages/geoprocessing/scripts/build/webpack.config.js
+++ packages/geoprocessing/scripts/build/webpack.config.js
@@ -87,6 +87,18 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
     ],
   },
 };

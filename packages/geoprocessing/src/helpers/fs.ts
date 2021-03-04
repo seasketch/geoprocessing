befda92b38e6a297f89acc15aafc7142b9fbb97f
Tim Welch <tim.j.welch@gmail.com>
2021-03-04 10:51:10 -0800

    Drop toJsonFile console statements

--- packages/geoprocessing/src/helpers/fs.ts
+++ packages/geoprocessing/src/helpers/fs.ts
@@ -1,9 +1,7 @@
 import fs from "fs";
 
 export function toJsonFile(data, filename) {
-  fs.writeFile(filename, JSON.stringify(data, null, 2), (err) =>
-    err
-      ? console.error("Error", err)
-      : console.info(`Successfully wrote ${filename}`)
-  );
+  fs.writeFile(filename, JSON.stringify(data, null, 2), (err) => {
+    if (err) throw new Error(err.message);
+  });
 }

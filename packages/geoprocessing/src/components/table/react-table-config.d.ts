01264031885b68c709eed5e532503edffa097be2
Tim Welch <tim.j.welch@gmail.com>
2021-03-12 10:07:28 -0800

    Document types moar

--- packages/geoprocessing/src/components/table/react-table-config.d.ts
+++ packages/geoprocessing/src/components/table/react-table-config.d.ts
@@ -50,7 +50,15 @@ import {
 } from "react-table";
 
 declare module "react-table" {
-  // take this file as-is, or comment out the sections that don't apply to your plugin configuration
+  /**
+   * Types for Table component.  As the library is plugin-based, so are the community types
+   * This module combines the @types/react-table base and plugin types and re-exports them with the same name
+   * This is possible through "interface merging", as long as the type signature stays the same
+   * The Table component that imports 'react-table' will find and use this extended module automatically
+   * Custom properties we support on our Table component are also included.
+   * Adjust this module as feature needs change or if react-library has breaking changes!
+   * Unused plugings are commented out
+   */
 
   export interface TableOptions<D extends object>
     extends UseExpandedOptions<D>,
@@ -62,9 +70,7 @@ declare module "react-table" {
       // UseRowSelectOptions<D>,
       // UseRowStateOptions<D>,
       UseSortByOptions<D> {
-    // note that having Record here allows you to add anything to the options, this matches the spirit of the
-    // underlying js library, but might be cleaner if it's replaced by a more specific type that matches your
-    // feature set, this is a safe default.
+    // Use this if you want to allow any prop to passed to the Table component, but specific is good
     // Record<string, any>
     /** Optional method to pass style.  Added to table element */
     className?: string;

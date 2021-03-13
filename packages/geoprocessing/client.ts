4456df7e7ed4e6f4497978763b0aa5c2d9560538
Tim Welch <tim.j.welch@gmail.com>
2021-03-12 17:37:49 -0800

    move client back outside src for now

---
+++ packages/geoprocessing/client.ts
@@ -0,0 +1,12 @@
+// Top-level module for importing client functions
+export { default as Card } from "./src/components/Card";
+export { default as ResultsCard } from "./src/components/ResultsCard";
+export { useFunction } from "./src/hooks/useFunction";
+export { default as SketchAttributesCard } from "./src/components/SketchAttributesCard";
+export { default as ReportDecorator } from "./src/components/ReportDecorator";
+export { default as Skeleton } from "./src/components/Skeleton";
+
+export { default as Table } from "./src/components/table/Table";
+export { default as ReportTable } from "./src/components/ReportTable";
+export { Checkbox as Checkbox } from "./src/components/Checbox";
+export { intersect } from "./src/spatial/IntersectHelper";

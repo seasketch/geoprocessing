bbcca7c1d7f65ad002ddcf1a17c025ca9a86eb8c
Tim Welch <tim.j.welch@gmail.com>
2021-03-18 12:32:20 -0700

    reorganize top-level client and export additional pieces

--- packages/geoprocessing/client.ts
+++ packages/geoprocessing/client.ts
@@ -1,10 +1,22 @@
 // Top-level module for importing client functions
+
+/* Storybook helper components */
+export { default as ReportDecorator } from "./src/components/ReportDecorator";
+export { default as ReportCardDecorator } from "./src/components/ReportCardDecorator";
+
+/* Helpers */
+export { intersect } from "./src/spatial/IntersectHelper";
+
+/* Hooks */
+export { default as useSketchProperties } from "./src/hooks/useSketchProperties";
+export { useFunction } from "./src/hooks/useFunction";
+export { default as useCheckboxes } from "./src/hooks/useCheckboxes";
+
+/* Components */
+export { default as Skeleton } from "./src/components/Skeleton";
 export { default as Card } from "./src/components/Card";
 export { default as ResultsCard } from "./src/components/ResultsCard";
-export { useFunction } from "./src/hooks/useFunction";
 export { default as SketchAttributesCard } from "./src/components/SketchAttributesCard";
-export { default as ReportDecorator } from "./src/components/ReportDecorator";
-export { default as Skeleton } from "./src/components/Skeleton";
 
 export {
   default as Table,
@@ -19,5 +31,5 @@ export {
   FilterSelectTableStyled,
 } from "./src/components/table/FilterSelectTable";
 export { default as ReportTable } from "./src/components/ReportTable";
+
 export { default as CheckboxGroup } from "./src/components/checkbox/CheckboxGroup";
-export { intersect } from "./src/spatial/IntersectHelper";

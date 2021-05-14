1665894f7a987cfc30951c6d084ec1782b16ffc6
Tim Welch <tim.j.welch@gmail.com>
2021-05-14 11:54:41 -0700

    improve osm-land-prep script

--- packages/gp-clip-ocean/data/osm-land-prep.sh
+++ packages/gp-clip-ocean/data/osm-land-prep.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Download and unpack
-if [ ! -d "src/land-polygons-complete-4326.zip"]; then
-  wget https://osmdata.openstreetmap.de/download/land-polygons-complete-4326.zip
+if [ ! -f "src/land-polygons-complete-4326.zip" ]; then
+  (cd src && curl -O https://osmdata.openstreetmap.de/download/land-polygons-complete-4326.zip)
 fi
+
 if [ ! -d "src/land-polygons-complete-4326" ]; then
-  unzip land-polygons-complete-4326.zip
+  unzip src/land-polygons-complete-4326.zip -d src
 fi
 
-# Import
-shp2pgsql -D -s 4326 src/land-polygons-complete-4326/land_polygons.shp osm_land | psql
+if [ -d "src/land-polygons-complete-4326" ]; then
+  # Import
+  shp2pgsql -D -s 4326 src/land-polygons-complete-4326/land_polygons.shp osm_land | psql
 
-# Create spatial index
-psql -t <<SQL
+  # Create spatial index
+  psql -t <<SQL
   CREATE INDEX ON land USING gist(geom);
 SQL
 
-# Subdivide into new table
-psql -f ./osm-land-subdivide.sql
+  # Subdivide into new table
+  psql -f ./osm-land-subdivide.sql
+else
+  echo "Unable to load data, download failed"
+fi

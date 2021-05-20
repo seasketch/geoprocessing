0056f975dd9e55e5cc3cac438a0f376bc996de7e
Tim Welch <tim.j.welch@gmail.com>
2021-05-20 16:36:08 -0700

    ✨ add booleanOverlap helper

---
+++ packages/geoprocessing/src/helpers/bboxOverlap.ts
@@ -0,0 +1,6 @@
+import { BBox } from "geojson";
+
+/** Returns whether bounding box A overlaps with or touches bounding box B */
+export function bboxOverlap(a: BBox, b: BBox) {
+  return a[2] >= b[0] && b[2] >= a[0] && a[3] >= b[1] && b[3] >= a[1];
+}
--- packages/geoprocessing/src/helpers/index.ts
+++ packages/geoprocessing/src/helpers/index.ts
@@ -3,5 +3,6 @@ export * from "./fs";
 export * from "./types";
 export * from "./sketch";
 export * from "./units";
+export * from "./bboxOverlap";
 export { default as intersect } from "./intersect";
 export { default as difference } from "./difference";

3812ec889b8366283f1948897f9d57a486395aa3
Tim Welch <tim.j.welch@gmail.com>
2021-06-09 14:17:06 -0700

    🔥 remove extra log messages

--- packages/geoprocessing/scripts/testing/index.test.ts
+++ packages/geoprocessing/scripts/testing/index.test.ts
@@ -13,52 +13,42 @@ import {
 // examples are loaded from fixture folder in packages/geoprocessing/examples
 describe("Example load utilities", () => {
   test("can get both sketches and collections", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExampleSketchAll();
     expect(examples.length).toBe(6);
   });
   test("can get just polygon sketches and collections", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExamplePolygonSketchAll();
     expect(examples.length).toBe(2);
   });
   test("can get just linestring sketches and collections", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExampleLineStringSketchAll();
     expect(examples.length).toBe(2);
   });
   test("can get just point sketches and collections", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExamplePointSketchAll();
     expect(examples.length).toBe(2);
   });
   test("can filter examples by name", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExampleSketchAll("sketch_line");
     expect(examples.length).toBe(1);
   });
   test("can get just collections", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExampleSketchCollections();
     expect(examples.length).toBe(3);
   });
   test("can get just sketches", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExampleSketches();
     expect(examples.length).toBe(3);
   });
   test("can get just sketch polygons", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExamplePolygonSketches();
     expect(examples.length).toBe(1);
   });
   test("can get just sketch linestrings", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExampleLineStringSketches();
     expect(examples.length).toBe(1);
   });
   test("can get just sketch points", async () => {
-    console.log("cwd", process.cwd());
     const examples = await getExamplePointSketches();
     expect(examples.length).toBe(1);
   });

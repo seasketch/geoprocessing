50a48a3c4a25bf458fc46e7750bf175f36d6b581
Tim Welch <tim.j.welch@gmail.com>
2021-06-24 19:29:12 -0700

    ✨ add keyBy and helper function unit test

--- packages/geoprocessing/src/helpers/functions.ts
+++ packages/geoprocessing/src/helpers/functions.ts
@@ -5,7 +5,18 @@ export const groupBy = <T, K extends keyof any>(
 ) =>
   list.reduce((previous, currentItem) => {
     const group = getKey(currentItem);
-    if (!previous[group]) previous[group] = [];
-    previous[group].push(currentItem);
-    return previous;
+    if (!previous[group]) {
+      return { ...previous, [group]: [currentItem] };
+    }
+    return { ...previous, [group]: previous[group].concat(currentItem) };
   }, {} as Record<K, T[]>);
+
+/** Similar to lodash keyBy */
+export const keyBy = <T, K extends keyof any>(
+  list: T[],
+  getKey: (item: T) => K
+) =>
+  list.reduce((previous, currentItem) => {
+    const group = getKey(currentItem);
+    return { ...previous, [group]: currentItem };
+  }, {} as Record<K, T>);

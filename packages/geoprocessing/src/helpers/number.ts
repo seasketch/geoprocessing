d99a8d43606cba93e08e67100f7ee51cf051df15
Tim Welch <tim.j.welch@gmail.com>
2021-07-08 16:23:39 -0700

    ✨ add percentLower and roundLower number formatting helpers

--- packages/geoprocessing/src/helpers/number.ts
+++ packages/geoprocessing/src/helpers/number.ts
@@ -1,4 +1,28 @@
 /** Rounds a number to a fixed precision  */
-export function roundDecimal(value: number, places: number) {
-  return +value.toFixed(places);
-}
+export const roundDig = (value: number, decimals = 1) => {
+  return Number(
+    Math.round(parseFloat(`${value}e${decimals}`)) + `e-${decimals}`
+  );
+};
+
+/** Formats number value as percent string with special handling of minimum bound */
+export const percentLower = (
+  val: number,
+  { lower, digits } = { lower: 0.001, digits: 1 }
+) => {
+  const PercentFormatter = new Intl.NumberFormat("en", {
+    style: "percent",
+    maximumFractionDigits: digits,
+  });
+
+  return val < lower
+    ? `< ${PercentFormatter.format(lower)}`
+    : PercentFormatter.format(val);
+};
+
+/** Formats number to string, rounding decimal to number of digits, with special handling of minimum bound */
+export const roundLower = (val: number, { lower } = { lower: 1 }) => {
+  const NumberFormatter = new Intl.NumberFormat("en", { style: "decimal" });
+
+  return val < lower ? `< ${lower}` : NumberFormatter.format(roundDig(val, 1));
+};

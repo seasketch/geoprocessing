367a4dfa1959f1ac2f119f6ef34a5c606905aca1
Tim Welch <tim.j.welch@gmail.com>
2021-07-27 10:05:02 -0700

    polyfill TextDecoder TextEncoder

--- packages/geoprocessing/src/datasources/flatgeobuf.ts
+++ packages/geoprocessing/src/datasources/flatgeobuf.ts
@@ -3,8 +3,19 @@ import { BBox } from "../types";
 
 //@ts-ignore
 global["ReadableStream"] = ReadableStream;
+
+// support older nodejs
+if (typeof TextDecoder === "undefined" && typeof require !== "undefined") {
+  (global as any).TextDecoder = require("util").TextDecoder;
+}
 //@ts-ignore
 global["TextDecoder"] = TextDecoder;
+
+// support older nodejs
+if (typeof TextEncoder === "undefined" && typeof require !== "undefined") {
+  (global as any).TextEncoder = require("util").TextEncoder;
+}
+//@ts-ignore
 global["TextEncoder"] = TextEncoder;
 
 export { deserialize } from "flatgeobuf/lib/cjs/geojson";

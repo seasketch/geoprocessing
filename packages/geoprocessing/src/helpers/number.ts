b4def7739bb391fdb3c2a5632d5d4981f0afaee5
Tim Welch <tim.j.welch@gmail.com>
2021-10-13 11:00:19 -0700

    percentLower: add override

--- packages/geoprocessing/src/helpers/number.ts
+++ packages/geoprocessing/src/helpers/number.ts
@@ -8,16 +8,27 @@ export const roundDecimal = (value: number, decimals = 1) => {
 /** Formats number value as percent string with special handling of minimum bound */
 export const percentLower = (
   val: number,
-  { lower, digits } = { lower: 0.001, digits: 1 }
+  options: { lower: number; digits: number; lowerOverride?: string } = {
+    lower: 0.001,
+    digits: 1,
+  }
 ) => {
+  const { lower, digits, lowerOverride } = options;
+
   const PercentFormatter = new Intl.NumberFormat("en", {
     style: "percent",
     maximumFractionDigits: digits,
   });
 
-  return val < lower && val !== 0
-    ? `<${PercentFormatter.format(lower)}`
-    : PercentFormatter.format(val);
+  if (val < lower) {
+    if (lowerOverride) {
+      return `${lowerOverride}`;
+    } else {
+      return `< ${PercentFormatter.format(lower)}`;
+    }
+  } else {
+    return PercentFormatter.format(val);
+  }
 };
 
 /** Formats number to string, rounding decimal to number of digits, with special handling of minimum bound */

0fedddb5fa32fa610f9270f1c7716a3fcdea9a48
Tim Welch <tim.j.welch@gmail.com>
2021-10-20 14:20:56 -0700

    ⚡️ areaByClassVector: migrate to faster intersect pattern

--- packages/geoprocessing/src/metrics/areaByClass.ts
+++ packages/geoprocessing/src/metrics/areaByClass.ts
@@ -11,8 +11,7 @@ import {
   RasterDatasourceMeta,
   ClassDatasourceMeta,
   loadCogWindow,
-  fgBoundingBox,
-  fgbDeserialize,
+  fgbFetchAll,
 } from "..";
 import { Georaster } from "../types/georaster";
 import logger from "../util/logger";
@@ -21,45 +20,57 @@ import logger from "../util/logger";
 import geoblaze from "geoblaze";
 import area from "@turf/area";
 import combine from "@turf/combine";
+import bbox from "@turf/bbox";
 
 /**
  * returns areaByClass metric calculation.  Class-based features are fetched
  * from flatgeobuf datasource and only the portion within features (e.g. sketch
- * collection) are included in calculation
+ * collection) are included in calculation.
  */
 export async function areaByClassVector<P extends ClassFeatureProps>(
+  /** collection of features each with one assigned class */
   features: FeatureCollection<Polygon>,
-  box: BBox,
-  config: VectorDatasourceMeta
+  /** The feature property containing the class name */
+  className: string,
+  config: VectorDatasourceMeta,
+  /** bounding fox of the features */
+  box?: BBox
 ) {
-  // Intersect polys one at a time as they come over the wire, maintaining properties
   try {
     const featureMulti = (combine(features) as FeatureCollection<MultiPolygon>)
       .features[0];
-    const iter = fgbDeserialize(config.vectorUrl, fgBoundingBox(box));
 
-    let clippedFeatures: Feature<Polygon, P>[] = [];
+    // Fetch all features in order to intersect them in groups, much faster than one at a time
+    const featuresToIntersect = await fgbFetchAll<Feature<Polygon>>(
+      config.vectorUrl,
+      box || bbox(features)
+    );
+    const type = className || "type";
+    type VectorFeature = Feature<Polygon>;
+
+    // Group features by type up front.  We cannot simply intersect them all in one shot because we will lose the knowledge of which clipped poly has which class
+    const featuresByClass = featuresToIntersect.reduce<
+      Record<string, VectorFeature[]>
+    >((acc, curFeature) => {
+      const className: string = curFeature.properties?.className;
+      return {
+        ...acc,
+        [className]: acc[className]
+          ? acc[className].concat(curFeature)
+          : [curFeature],
+      };
+    }, {});
+
+    // Bulk intersect each group, its cheaper for intersects underlying sweepline algorithm to process them all in one pass
     let areaByClass: Record<string, number> = {};
-    // @ts-ignore
-    for await (const featureToClip of iter) {
-      const polyClipped = intersect(featureToClip, featureMulti, {
-        properties: featureToClip.properties,
-      }) as Feature<Polygon, P>;
-      if (polyClipped) {
-        clippedFeatures.push(polyClipped);
-
-        // Sum total area by class ID within feature
-        const polyArea = area(polyClipped);
-        areaByClass[polyClipped.properties.class_id] =
-          areaByClass.hasOwnProperty(polyClipped.properties.class_id)
-            ? areaByClass[polyClipped.properties.class_id] + polyArea
-            : polyArea;
-      }
-    }
+    Object.keys(featuresByClass).map((fbc) => {
+      const clippedMultipoly = intersect(featureMulti, featuresByClass[fbc]);
+      if (clippedMultipoly) areaByClass[fbc] = area(clippedMultipoly);
+    });
 
     return areaByClass;
-  } catch (err) {
-    logger.error("habitat error", err);
+  } catch (err: unknown) {
+    logger.error("habitat error", err as Error);
     throw err;
   }
 }
@@ -79,8 +90,8 @@ export async function areaByClassRaster(
     const areaByClass = await rasterClassStats(raster, config, fc.features);
 
     return areaByClass;
-  } catch (err) {
-    logger.error("habitat error", err);
+  } catch (err: unknown) {
+    logger.error("habitat error", err as Error);
     throw err;
   }
 }

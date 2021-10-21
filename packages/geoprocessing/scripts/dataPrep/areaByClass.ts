8268879fd8cb7737dfefc525594b6f18d3e50fbc
Tim Welch <tim.j.welch@gmail.com>
2021-10-21 16:11:29 -0700

    cleanup debug

--- packages/geoprocessing/scripts/dataPrep/areaByClass.ts
+++ packages/geoprocessing/scripts/dataPrep/areaByClass.ts
@@ -2,10 +2,6 @@ import fs from "fs";
 import { calcAreaStats } from "../../src/metrics/calcAreaByClass";
 import { FeatureCollection, Polygon } from "../../src/types";
 
-const INPATH = "legislated.json";
-const OUTPATH = "legislatedStats.json";
-const ATTRIB = "Type";
-
 export function areaByClass(
   inPath: string,
   outPath: string,
@@ -15,9 +11,6 @@ export function areaByClass(
   const fc = JSON.parse(fs.readFileSync(inPath).toString());
   const features = fc as FeatureCollection<Polygon>;
   const stats = calcAreaStats(features, { classProperty });
-  console.log();
-  console.log(JSON.stringify(stats));
-  console.log();
   fs.writeFile(outPath, JSON.stringify(stats, null, 2), (err) =>
     err
       ? console.error("Error", err)

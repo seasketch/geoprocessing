95232d3d0eab187355489dac2549b48aadfe215f
Tim Welch <tim.j.welch@gmail.com>
2021-10-29 20:39:39 -0700

    ✨ add minWidth, maxWidth to toolbox

--- packages/geoprocessing/src/toolbox/index.ts
+++ packages/geoprocessing/src/toolbox/index.ts
@@ -1,7 +1,13 @@
 import { overlap } from "./overlap";
+import { minWidth, maxWidth } from "./width";
+
+export * from "./overlap";
+export * from "./width";
 
 const toolbox = {
   overlap,
+  minWidth,
+  maxWidth,
 };
 
 export default toolbox;
---
+++ packages/geoprocessing/src/toolbox/width.ts
@@ -0,0 +1,53 @@
+import bbox from "@turf/bbox";
+import bboxPolygon from "@turf/bbox-polygon";
+import explode from "@turf/explode";
+import distance from "@turf/distance";
+import { AllGeoJSON, BBox } from "@turf/helpers";
+
+/**
+ * Returns the maximum width of the geojson or bbox
+ */
+export const maxWidth = (geojson: AllGeoJSON | BBox) => {
+  const box = (() => {
+    if (Array.isArray(geojson)) {
+      return geojson as BBox;
+    } else {
+      return bbox(geojson);
+    }
+  })();
+
+  const boxPoly = bboxPolygon(box);
+  const boxPoints = explode(boxPoly);
+  return Math.max(
+    distance(boxPoints.features[0], boxPoints.features[1], {
+      units: "degrees",
+    }),
+    distance(boxPoints.features[1], boxPoints.features[2], {
+      units: "degrees",
+    })
+  );
+};
+
+/**
+ * Returns the minimum width of the bounding box of given feature
+ */
+export const minWidth = (geojson: AllGeoJSON | BBox) => {
+  const box = (() => {
+    if (Array.isArray(geojson)) {
+      return geojson as BBox;
+    } else {
+      return bbox(geojson);
+    }
+  })();
+
+  const boxPoly = bboxPolygon(box);
+  const boxPoints = explode(boxPoly);
+  return Math.min(
+    distance(boxPoints.features[0], boxPoints.features[1], {
+      units: "degrees",
+    }),
+    distance(boxPoints.features[1], boxPoints.features[2], {
+      units: "degrees",
+    })
+  );
+};

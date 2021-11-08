4a058c8b92ac201f9218f3b1b4b82895ee0a7c25
Tim Welch <tim.j.welch@gmail.com>
2021-11-08 11:24:18 -0800

    🍱 export ReportContext in client

--- packages/geoprocessing/client.ts
+++ packages/geoprocessing/client.ts
@@ -6,6 +6,7 @@ export * from "./src/types";
 export * from "./src/components";
 // Fixes interface export issue with webpack - https://github.com/webpack/webpack/issues/7378#issuecomment-683894656
 export type { FilterSelectTableOptions } from "./src/components/table/FilterSelectTable";
+export { default as ReportContext } from "./src/ReportContext";
 
 export * from "./src/hooks";
 

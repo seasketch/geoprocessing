c4d35eeaffc69a7b6d0dbc43482f08532dfc4238
Tim Welch <tim.j.welch@gmail.com>
2021-11-16 13:16:38 -0800

    ✨ percentWithEdge add support for formatting value if matches lower bound. percentGoalWithEdge add passthru options to percentWithEdge

--- packages/geoprocessing/src/helpers/number.ts
+++ packages/geoprocessing/src/helpers/number.ts
@@ -5,37 +5,44 @@ export const roundDecimal = (value: number, decimals = 1) => {
   );
 };
 
+interface PercentEdgeOptions {
+  /** Number of decimal digits to round value to if is within lower or upper edge range.  defaults to 1.  Override with this option */
+  digits?: number;
+  /** Number of decimal digits to round value to if exactly matches lowerBound, defaults to 0 (whole number)  */
+  digitsIfMatchLower?: number;
+  /** Define a lower value bound.  Defaults to 0 (zero). */
+  lowerBound?: number;
+  /** Enable special formatting of values from lowerBound up to lower value.  Defaults to .001 aka 1/10 of a percent */
+  lower?: number;
+  /** Optional string value to display if between zero and lower.  Overrides default special handling, no use of percent formatter.  Example - "< 0.1% for real" */
+  lowerOverride?: string;
+  /** Define an upper value bound.  Enable special formatting of values from upper to upperBound */
+  upperBound?: number;
+  /** Define lower bound to upper value.  Enable special formatting of values from upper to upperBound */
+  upper?: number;
+  /** Optional string value to display if between upper and upperBound.  Overrides default special handling, no use of percent formatter.  Example - "almost 20%, keep going!" */
+  upperOverride?: string;
+}
+
 /**
- * Formats number value as percent string, with special handling of numbers
- * within some edge of lower or upper bounds.  Defaults to handle only lower
- * with lowerBound = 0 and lower = .001. All bound values are expected to be
- * in decimal percent.  So 1/10th of a percent is .001
+ * Special percent formatter designed to produce readable percent values for
+ * display with special handling of numbers within some edge range of
+ * user-defined lower or upper bounds.  Defaults to handle only lower edge with
+ * lowerBound = 0 and lower = .001. All bound values are expected to be in
+ * decimal percent.  So 1/10th of a percent is .001
  */
 export const percentWithEdge = (
   val: number,
-  options: {
-    /** Special handling of value defaults to rounding to 1 maximumFractionDigit.  Override with this option */
-    digits?: number;
-    /** Define a lower value bound.  Defaults to 0 (zero). */
-    lowerBound?: number;
-    /** Enable special formatting of values from lowerBound up to lower value.  Defaults to .001 aka 1/10 of a percent */
-    lower?: number;
-    /** Optional string value to display if between zero and lower.  Overrides default special handling, no use of percent formatter.  Example - "< 0.1% for real" */
-    lowerOverride?: string;
-    /** Define an upper value bound.  Enable special formatting of values from upper to upperBound */
-    upperBound?: number;
-    /** Define lower bound to upper value.  Enable special formatting of values from upper to upperBound */
-    upper?: number;
-    /** Optional string value to display if between upper and upperBound.  Overrides default special handling, no use of percent formatter.  Example - "almost 20%, keep going!" */
-    upperOverride?: string;
-  } = {
+  options: PercentEdgeOptions = {
     digits: 1,
+    digitsIfMatchLower: 0,
     lower: 0.001,
     lowerBound: 0,
   }
 ) => {
   const {
     digits = 1,
+    digitsIfMatchLower = 0,
     lower = 0.001,
     lowerBound = 0,
     lowerOverride,
@@ -49,7 +56,14 @@ export const percentWithEdge = (
     maximumFractionDigits: digits,
   });
 
-  if (val > lowerBound && val < lower) {
+  const MatchPercentFormatter = new Intl.NumberFormat("en", {
+    style: "percent",
+    maximumFractionDigits: digitsIfMatchLower,
+  });
+
+  if (val === lowerBound) {
+    return MatchPercentFormatter.format(val);
+  } else if (val > lowerBound && val < lower) {
     if (lowerOverride) {
       return lowerOverride;
     } else {
@@ -79,12 +93,16 @@ export const percentGoalWithEdge = (
   /** Actual percent value */
   val: number,
   /** Goal percent value */
-  goal: number
-) =>
-  percentWithEdge(val, {
+  goal: number,
+  /** Override options passed to percentWithEdge, supports same parameters */
+  options?: PercentEdgeOptions
+) => {
+  return percentWithEdge(val, {
     upperBound: goal,
     upper: goal - 0.001,
+    ...(options || {}),
   });
+};
 
 /** Formats number to string, rounding decimal to number of digits, with special handling of minimum bound */
 export const roundLower = (val: number, { lower } = { lower: 1 }) => {

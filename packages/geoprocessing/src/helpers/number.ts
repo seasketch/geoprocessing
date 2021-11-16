a0cadefab009104d031f4e62de38433a9240d574
Tim Welch <tim.j.welch@gmail.com>
2021-11-16 11:40:33 -0800

    ✨ change percentLower to percentWithEdge supporting lower and upper bound handling

--- packages/geoprocessing/src/helpers/number.ts
+++ packages/geoprocessing/src/helpers/number.ts
@@ -6,27 +6,63 @@ export const roundDecimal = (value: number, decimals = 1) => {
 };
 
 /**
- * Formats number value as percent string with special handling of numbers greater than zero up to lower
+ * Formats number value as percent string with special handling of numbers approaching z than zero up to lower
+ * All bound values are expected to be in decimal percent.  So 1/10th of a percent is .001
  */
-export const percentLower = (
+export const percentWithEdge = (
   val: number,
-  options: { lower?: number; digits?: number; lowerOverride?: string } = {
-    lower: 0.001,
+  options: {
+    /** Special handling of value defaults to rounding to 1 maximumFractionDigit.  Override with this option */
+    digits?: number;
+    /** Define a lower value bound.  Defaults to 0 (zero). */
+    lowerBound?: number;
+    /** Enable special formatting of values from lowerBound up to lower value.  Defaults to .001 aka 1/10 of a percent */
+    lower?: number;
+    /** Optional string value to display if between zero and lower.  Overrides default special handling, no use of percent formatter.  Example - "< 0.1% for real" */
+    lowerOverride?: string;
+    /** Define an upper value bound.  Enable special formatting of values from upper to upperBound */
+    upperBound?: number;
+    /** Define lower bound to upper value.  Enable special formatting of values from upper to upperBound */
+    upper?: number;
+    /** Optional string value to display if between upper and upperBound.  Overrides default special handling, no use of percent formatter.  Example - "almost 20%, keep going!" */
+    upperOverride?: string;
+  } = {
     digits: 1,
+    lower: 0.001,
+    lowerBound: 0,
   }
 ) => {
-  const { lower = 0.001, digits = 1, lowerOverride } = options;
+  const {
+    digits = 1,
+    lower = 0.001,
+    lowerBound = 0,
+    lowerOverride,
+    upper,
+    upperBound,
+    upperOverride,
+  } = options;
 
   const PercentFormatter = new Intl.NumberFormat("en", {
     style: "percent",
     maximumFractionDigits: digits,
   });
 
-  if (val > 0 && val < lower) {
+  if (val > lowerBound && val < lower) {
     if (lowerOverride) {
-      return `${lowerOverride}`;
+      return lowerOverride;
     } else {
       return `< ${PercentFormatter.format(lower)}`;
+    }
+  } else if (
+    upper !== undefined &&
+    upperBound !== undefined &&
+    val < upperBound &&
+    val > upper
+  ) {
+    if (upperOverride) {
+      return upperOverride;
+    } else {
+      return PercentFormatter.format(upper);
     }
   } else {
     return PercentFormatter.format(val);

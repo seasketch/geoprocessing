273b085926ae2a8dcc21d151fd4b940b0be7b019
Tim Welch <tim.j.welch@gmail.com>
2022-01-25 21:19:30 -0800

    🐛 fix dataproviders top-level module

--- packages/geoprocessing/dataproviders.ts
+++ packages/geoprocessing/dataproviders.ts
@@ -1,20 +1,4 @@
 // Top-level module for data providers
 // Kept separate from main module to avoid bundling issues
 
-// Base types
-export * from "./src/types";
-
-// Helpers - not all of them
-export * from "./src/helpers/units";
-export * from "./src/helpers/keyBy";
-export * from "./src/helpers/groupBy";
-export * from "./src/helpers/string";
-export * from "./src/helpers/number";
-export * from "./src/helpers/types";
-export {
-  toSketchArray,
-  toNullSketch,
-  toNullSketchArray,
-  getUserAttribute,
-  getJsonUserAttribute,
-} from "./src/helpers/sketch";
+export * from "./src/dataproviders";

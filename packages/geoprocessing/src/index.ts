6ba150828b793fd0cc18129a4ca3490922b92059
Tim Welch <tim.j.welch@gmail.com>
2022-02-01 16:05:16 -0800

    🐛 export iucn from top-level module

--- packages/geoprocessing/src/index.ts
+++ packages/geoprocessing/src/index.ts
@@ -1,10 +1,11 @@
 // top-level module for geoprocessing library, primarily used for node environmnent
-export * from "./types";
-export * from "./helpers";
-export * from "./datasources";
-export * from "./metrics";
-export * from "./toolbox";
 export * from "./aws";
+export * from "./datasources";
+export * from "./helpers";
+export * from "./iucn";
+export * from "./metrics";
 export * from "./testing";
+export * from "./toolbox";
+export * from "./types";
 
 export { default as version } from "../package.json";

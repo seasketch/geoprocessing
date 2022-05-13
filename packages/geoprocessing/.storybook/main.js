c667dee48b92b077968e23ab11b19534eaa2f0be
Tim Welch <tim.j.welch@gmail.com>
2022-05-13 11:45:38 -0700

    storybook fix project paths check

--- packages/geoprocessing/.storybook/main.js
+++ packages/geoprocessing/.storybook/main.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 
 const baseStories = [
   "../src/**/*.stories.tsx",
@@ -9,18 +10,22 @@ const baseStories = [
 const projectStories = [];
 
 if (process.env.PROJECT_PATH) {
-  if ((process.env.PROJECT_PATH, "src/clients")) {
-    console.log("src/clients directory not found, skipping");
+  if (fs.existsSync(path.join(process.env.PROJECT_PATH, "src/clients"))) {
+    console.log("Clients path found, adding to stories");
     projectStories.push(
       path.join(process.env.PROJECT_PATH, "src/clients") + "/**/*.stories.tsx"
     );
+  } else {
+    console.log("Clients path not found, skipping");
   }
-  if ((process.env.PROJECT_PATH, "src/components")) {
-    console.log("src/components directory not found, skipping");
+  if (fs.existsSync(path.join(process.env.PROJECT_PATH, "src/components"))) {
+    console.log("Components path found, adding to stories");
     projectStories.push(
       path.join(process.env.PROJECT_PATH, "src/components") +
         "/**/*.stories.tsx"
     );
+  } else {
+    console.log("Components path not found, skipping");
   }
 }
 

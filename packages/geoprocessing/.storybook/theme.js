61e5ac50b65c570843bc5458c9e37f076eca276a
Tim Welch <tim.j.welch@gmail.com>
2022-05-19 14:24:47 -0700

    Stub storybook theme and point home to main wiki

---
+++ packages/geoprocessing/.storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "SeaSketch Geoprocessing",
+  brandUrl: "https://github.com/seasketch/geoprocessing/wiki",
+  brandImage: "https://www.seasketch.org/images/logo.png",
+  brandTarget: "_self",
+});

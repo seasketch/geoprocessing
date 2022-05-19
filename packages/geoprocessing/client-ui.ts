11269c5c98ec2298930d4ba43ffe4d6fb3554e66
Tim Welch <tim.j.welch@gmail.com>
2022-05-19 12:16:18 -0700

    Typedoc support (#111)
    * Typedoc support
    
    * add docs publish action

--- packages/geoprocessing/client-ui.ts
+++ packages/geoprocessing/client-ui.ts
@@ -1,4 +1,10 @@
-// Top-level module for UI components
+/**
+ * `client-ui` provides all of the core React UI components for geoprocessing
+ * projects, including the building blocks for creating custom UI components
+ * needed by a project. Run the `start-storybook` command to explore them
+ * interactively.
+ * @packageDocumentation
+ */
 
 export * from "./src/components";
 // Fixes interface export issue with webpack - https://github.com/webpack/webpack/issues/7378#issuecomment-683894656
---
+++ packages/geoprocessing/scripts/index.ts
@@ -0,0 +1,5 @@
+export * from "./util";
+export * from "./testing";
+export * from "./template";
+export * from "./manifest";
+export * from "./types";

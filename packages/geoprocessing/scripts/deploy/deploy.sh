15aef1479b6fec49a7c716d77be09a4fe528da6f
Tim Welch <tim.j.welch@gmail.com>
2022-06-28 19:55:19 -0700

    switch to aws-cdk to run 2.x, ensure post deploy scripts only run if successful

--- packages/geoprocessing/scripts/deploy/deploy.sh
+++ packages/geoprocessing/scripts/deploy/deploy.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 export PROJECT_PATH=$(pwd)
 cd node_modules/@seasketch/geoprocessing
-npx cdk --app "node dist/scripts/deploy/createStack.js" --outputs-file ../../../cdk-outputs.json --require-approval never deploy
-node dist/scripts/deploy/fixIndexHtmlCacheControl.js
-node dist/scripts/deploy/beep_when_done.js
+npx aws-cdk --app "node dist/scripts/deploy/createStack.js" --outputs-file ../../../cdk-outputs.json --require-approval never deploy || node dist/scripts/deploy/fixIndexHtmlCacheControl.js || node dist/scripts/deploy/beep_when_done.js
--- packages/geoprocessing/scripts/deploy/destroy.sh
+++ packages/geoprocessing/scripts/deploy/destroy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export PROJECT_PATH=$(pwd)
 cd node_modules/@seasketch/geoprocessing
-npx cdk destroy
+npx aws-cdk destroy
 

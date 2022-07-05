038b40798cd098c0c37275b486f7c36043488bb0
Tim Welch <tim.j.welch@gmail.com>
2022-07-05 09:21:36 -0700

    drop beep that has never been beeped

--- packages/geoprocessing/scripts/deploy/deploy.sh
+++ packages/geoprocessing/scripts/deploy/deploy.sh
@@ -4,4 +4,3 @@ export PROJECT_PATH=$(pwd)
 cd node_modules/@seasketch/geoprocessing
 npx aws-cdk --app "node dist/scripts/deploy/createStack.js" --outputs-file ../../../cdk-outputs.json --require-approval never deploy
 node dist/scripts/deploy/fixIndexHtmlCacheControl.js
-node dist/scripts/deploy/beep_when_done.js

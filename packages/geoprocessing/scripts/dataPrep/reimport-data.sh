dcd1a682e089a8e490de400a27881bcbc8d983cf
Tim Welch <tim.j.welch@gmail.com>
2022-09-26 10:32:57 -0700

    fix param pass through

--- packages/geoprocessing/scripts/dataPrep/reimport-data.sh
+++ packages/geoprocessing/scripts/dataPrep/reimport-data.sh
@@ -2,7 +2,7 @@ export PROJECT_PATH=$(pwd)
 set -e
 
 echo "PROJECT_PATH: $PROJECT_PATH"
-echo "Datasource matcher: $1"
+echo "Datasource matcher: $2"
 
 # Default to assuming running from project, but check if running from within monorepo like example project
 GP_PATH="node_modules/@seasketch/geoprocessing"
@@ -15,4 +15,4 @@ echo "Starting local data server for raster import..."
 echo ""
 
 # Run web server, then script, and kill both when done
-npx http-server "$PROJECT_PATH/data/dist" -s -p 8001 & node "${GP_PATH}/dist/scripts/dataPrep/reimportData.js" $PROJECT_PATH $1 && kill $!
+npx http-server "$PROJECT_PATH/data/dist" -s -p 8001 & node "${GP_PATH}/dist/scripts/dataPrep/reimportData.js" $PROJECT_PATH $2 && kill $!

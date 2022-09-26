506d142f34fafe512cfb9cca5f578d8522da6305
Tim Welch <tim.j.welch@gmail.com>
2022-09-25 18:59:25 -0700

    add reimport-data bash

---
+++ packages/geoprocessing/scripts/dataPrep/reimport-data.sh
@@ -0,0 +1,17 @@
+export PROJECT_PATH=$(pwd)
+set -e
+
+echo "PROJECT_PATH: $PROJECT_PATH"
+
+a# Default to assuming running from project, but check if running from within monorepo like example project
+GP_PATH="node_modules/@seasketch/geoprocessing"
+if test -f "$GP_PATH"; then
+    GP_PATH="../geoprocessing"
+fi
+
+echo ""
+echo "Starting local data server for raster import..."
+echo ""
+
+# Run web server, then script, and kill both when done
+npx http-server "$PROJECT_PATH/data/dist" -s -p 8001 & node "${GP_PATH}/dist/scripts/dataPrep/reimportData.js" $PROJECT_PATH && kill $!

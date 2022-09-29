7d13cf6ce45257466d0cf27349868547f2ccef25
Tim Welch <tim.j.welch@gmail.com>
2022-09-29 11:14:29 -0700

    pass server on to project to start, unsuppress logging

--- packages/geoprocessing/scripts/dataPrep/import-data.sh
+++ packages/geoprocessing/scripts/dataPrep/import-data.sh
@@ -10,19 +10,4 @@ if test -f "$GP_PATH"; then
     GP_PATH="../geoprocessing"
 fi
 
-echo ""
-echo "Starting local web server on port ${PORT} for fetching COG rasters..."
-echo ""
-
-# Run web server, then script, and kill both when done or if exit early
-onINT() {
-echo "You exited early, killing server listening on port ${PORT} at PID $command1PID"
-kill -INT "$command1PID"
-exit
-}
-
-trap "onINT" SIGINT
-npx http-server "$PROJECT_PATH/data/dist" -s -p ${PORT} &
-command1PID="$!"
 node "${GP_PATH}/dist/scripts/dataPrep/importData.js" $PROJECT_PATH
-echo Done

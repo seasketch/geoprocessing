3cfb2ae348f9295605dad602c083530332ed9f4d
Tim Welch <tim.j.welch@gmail.com>
2023-02-11 10:24:31 -0800

    getFeatures: add external datasource and propertyFilter support

--- packages/geoprocessing/src/dataproviders/getFeatures.ts
+++ packages/geoprocessing/src/dataproviders/getFeatures.ts
@@ -1,4 +1,8 @@
-import { isInternalVectorDatasource } from "../datasources";
+import {
+  isExternalVectorDatasource,
+  isInternalVectorDatasource,
+  VectorDataSource,
+} from "../datasources";
 import { fgbFetchAll } from "./flatgeobuf";
 import {
   ExternalVectorDatasource,
@@ -13,27 +17,58 @@ interface VectorPropertyFilter {
 }
 
 /**
- * Returns features for a variety of datasources, with additional filter options
+ * Returns features for a variety of vector datasources and formats, with additional filter options
+ * Currently only supports internal flatgeobuf and external subdivided datasources
  */
 export async function getFeatures(
   datasource: InternalVectorDatasource | ExternalVectorDatasource,
+  /** url of datasource */
   url: string,
   options: {
+    /** Fetches features overlapping with bounding box */
     bbox?: BBox;
-    /** Filters features to those having one or more properties with one or more specific values. */
-    propertyFilters?: VectorPropertyFilter[];
-    /** Used for subdivided datasources to rebuild features again */
+    /** Filter features by property having one or more specific values */
+    propertyFilter?: VectorPropertyFilter;
+    /** Provide if you have subdivided dataset and want to rebuild (union) subdivided polygons based on having same value for this property name */
     unionProperty?: string;
   }
 ): Promise<Feature[]> {
-  let features: Feature[];
+  let features: Feature[] = [];
   if (isInternalVectorDatasource(datasource)) {
-    console.log("url", url);
+    // internal
     features = await fgbFetchAll<Feature>(url, options.bbox);
-  } else {
-    throw new Error(
-      `Expected vector datasource for ${datasource.datasourceId}`
-    );
+  } else if (isExternalVectorDatasource(datasource)) {
+    // external
+    if (!options.bbox) {
+      throw new Error(
+        `bbox option expected for ExternalVectorDatasource ${datasource.datasourceId}`
+      );
+    }
+    const vectorDs = new VectorDataSource(url);
+
+    // union subdivided polygons
+    if (datasource.formats.includes("subdivided")) {
+      if (options.unionProperty) {
+        features = (
+          await vectorDs.fetchUnion(options.bbox, options.unionProperty)
+        ).features;
+      } else {
+        features = await vectorDs.fetch(options.bbox);
+      }
+    } else {
+      throw new Error("Only external subdivided datasources supported");
+    }
   }
+
+  // filter by property value
+  if (options.propertyFilter) {
+    features = features.filter((curFeat) => {
+      if (!curFeat.properties) return false;
+      return options.propertyFilter?.values.includes(
+        curFeat.properties[options.propertyFilter.property]
+      );
+    });
+  }
+
   return features;
 }

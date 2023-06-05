a9280be46151d6bdf48ca03ea04170d025ca4437
Abby Vath Meyer <avmey@ucsb.edu>
2023-06-05 09:32:54 -0700

    Adds territorial waters editable diagram

---
+++ packages/geoprocessing/src/components/diagram/WatersBackgroundDetailed.tsx
@@ -0,0 +1,293 @@
+import React from "react";
+
+/**
+ * Creates detailed territorial waters svg to be used in WatersDiagram
+ *
+ * Original svg in geoprocessing repo under assets/img/WatersBackgroundDetailed.svg
+ */
+export function WatersBackgroundDetailed() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 700">
+      <defs>
+        <marker orient="auto" overflow="visible" refX="0" refY="0">
+          <path
+            fillRule="evenodd"
+            stroke="#000"
+            strokeWidth=".4pt"
+            markerStart="none"
+            d="M0 0l2-2-7 2 7 2-2-2z"
+          ></path>
+        </marker>
+        <marker orient="auto" overflow="visible" refX="0" refY="0">
+          <path
+            fillRule="evenodd"
+            stroke="#000"
+            strokeWidth=".8pt"
+            markerStart="none"
+            d="M0 0l-4 4 14-4-14-4 4 4z"
+          ></path>
+        </marker>
+        <marker orient="auto" overflow="visible" refX="0" refY="0">
+          <path
+            fillRule="evenodd"
+            stroke="#000"
+            strokeWidth=".8pt"
+            markerStart="none"
+            d="M0 0l4-4-14 4L4 4 0 0z"
+          ></path>
+        </marker>
+        <pattern
+          width="129.836"
+          height="79.817"
+          patternTransform="translate(50.276 605.972)"
+          patternUnits="userSpaceOnUse"
+        >
+          <path
+            style={{ marker: "none" }}
+            fill="#f4ff00"
+            fillOpacity="1"
+            fillRule="evenodd"
+            stroke="#000"
+            strokeDasharray="2, 2"
+            strokeDashoffset="0"
+            strokeLinecap="square"
+            strokeLinejoin="miter"
+            strokeMiterlimit="4"
+            strokeOpacity="1"
+            strokeWidth="1"
+            markerEnd="none"
+            markerMid="none"
+            markerStart="none"
+            d="M0.5 0.5H129.336V79.317H0.5z"
+            color="#000"
+            display="inline"
+            opacity="1"
+            overflow="visible"
+            visibility="visible"
+          ></path>
+        </pattern>
+      </defs>
+      <path
+        fill="#d3ebfa"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.014"
+        d="M495.614.303l.036 235.84s-78.01-63.51-87.777-65.676c-9.766-2.167-79.214-39.01-88.98-44.428-9.904-5.494-100.916-34.673-175.789-46.59-73.97-11.772-70.531-8.665-111.765-6.494C-4.419 74.837.956 69.705.956 69.705L.951.346z"
+      ></path>
+      <path
+        fill="#bfe3fa"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.007"
+        d="M493.03 229.001l.003 42.764-131.043-49.094-87.813-21.375-84.562-18.169-89.98-6.408-100.822.007-.008-105.84s46.71 2.293 54.205-2.142c10.84-6.415 63.962 2.134 63.962 2.134s137.856 27.852 168.04 39.544c19.197 7.438 126.843 52.377 126.843 52.377z"
+      ></path>
+      <path
+        fill="#abdcfc"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.008"
+        d="M492.555 270.771l-136.852-51.306-109.698-26.72-59.736-11.756-86.888-6.408-102.093 1.076.018 253.375 132.507 35.27 194.41-3.22 52.134 8.548 49.962 11.757 66.252 5.34z"
+      ></path>
+      <path
+        fill="#82cbfb"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.006"
+        d="M-1.807 427.038l2.035 60.74 77.28 13.324 96.125 33.804 173.222 36.147 48.323-19.457 23.759-14.519 31.53 4.622 42.21 16.702-.003-74.88-63.856-3.203-61.924-23.286-51.541-2.167-189.58 4.118z"
+      ></path>
+      <path
+        fill="#f4e2ba"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.019"
+        d="M494.4 553.132l.014 185.118-497.948-1.044 1.001-249.368 54.363 7.612 133.06 15.852s12.398 26.464 20.028 22.108c7.63-4.356 30.364-23.115 30.364-23.115l59.634 6.916s9.81 0 22.892 9.799c13.082 9.8 18.534 42.467 35.975 35.932 17.44-6.535 32.7-20.692 32.7-20.692s15.26-19.602 34.881-14.158c19.622 5.443 51.234 7.618 51.234 7.618z"
+      ></path>
+      <path
+        d="M267.882-570.952a18.71 33.142 0 01-18.406 33.137 18.71 33.142 0 01-19.002-32.066 18.71 33.142 0 0117.793-34.174 18.71 33.142 0 0119.576 30.962"
+        transform="rotate(122.196)"
+        style={{ marker: "none" }}
+        fill="#f4e2ba"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeDasharray="1, 1"
+        strokeDashoffset="1.1"
+        strokeLinecap="square"
+        strokeLinejoin="miter"
+        strokeMiterlimit="4"
+        strokeOpacity="1"
+        strokeWidth="1"
+        markerEnd="none"
+        markerMid="none"
+        markerStart="none"
+        color="#000"
+        display="inline"
+        overflow="visible"
+        visibility="visible"
+      ></path>
+      <path
+        fill="#8fd3ff"
+        fillOpacity="1"
+        fillRule="evenodd"
+        stroke="none"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.004"
+        d="M1.187 397.638l128.082 22.115 194.715 1 61.78 12.692 38.287 8.207 68.284 7.986v43l-85.383-11.16-46.755-21.656L318 457.908l-38.86 13.246-153.11-10.653L1.186 435.638z"
+      ></path>
+      <g
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1"
+        transform="matrix(1.16453 0 0 1.13322 -456.235 -99.774)"
+      >
+        <path d="M424.808 246.386l-4.467 4.062"></path>
+        <path d="M425 520.55v-275"></path>
+        <path d="M425.011 246.386l4.467 4.062"></path>
+      </g>
+      <path
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.38"
+        d="M313.849 521.484v-58.552l-4.13 5.323"
+      ></path>
+      <path
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.461"
+        d="M314.305 462.108l3.813 6.097"
+      ></path>
+      <path
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.435"
+        d="M308.755 451.871c.88.48 3.203 2.407 5.369 4.174"
+      ></path>
+      <path
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.473"
+        d="M318.894 451.666l-4.989 4.83"
+      ></path>
+      <path
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.214"
+        d="M314.296 456.852l.25-33.922-4.258 3.995"
+      ></path>
+      <path
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.362"
+        d="M314.412 422.44l5.39 3.748"
+      ></path>
+      <g
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1"
+        transform="matrix(-.9936 -.45931 -.4219 1.02155 861.019 100.289)"
+      >
+        <path d="M208.482 519.47v25.07"></path>
+        <path d="M208 519.55l-4 3.5"></path>
+        <path d="M208 519.3l4 3.5"></path>
+      </g>
+      <g
+        fill="none"
+        fillOpacity="0.75"
+        fillRule="evenodd"
+        stroke="#000"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        transform="matrix(1.57587 0 0 .64868 269.662 173.543)"
+      >
+        <path strokeWidth="1" d="M38.424 436.295l-4.467-4.06"></path>
+        <path strokeWidth="1.162" d="M38.616 65.434v371.522"></path>
+        <path strokeWidth="1" d="M38.627 436.295l4.468-4.06"></path>
+      </g>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeDasharray="4.01949, 4.01949"
+        strokeDashoffset="0"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1.005"
+        d="M0 487.05l39.943 6.508 143.665 16.924 7.731 13.67 7.731 9.113 4.51.65 3.221-1.301 28.346-22.783 57.982 7.16 10.308 1.953 10.952 5.208 9.663 9.764 9.02 16.273 7.086 10.414 4.51 2.604 5.154 1.302 6.442-1.953 11.597-6.51 12.884-8.461 9.664-9.113 7.73-6.51 10.309-4.556 8.375-.65 10.308 2.603 10.952 2.604 24.48 3.254h9.02l9.02 7.16 12.24 11.066v0"
+      ></path>
+      <path
+        fill="none"
+        stroke="#000"
+        strokeDasharray="4, 4"
+        strokeDashoffset="0"
+        strokeLinecap="butt"
+        strokeLinejoin="miter"
+        strokeOpacity="1"
+        strokeWidth="1"
+        d="M323.78 495.748l-3.002 4.503-.75 4.877 1.125 5.252 3.001 5.253 4.503 6.003 6.378 6.378 7.503 4.502 8.254 4.127 6.753 1.876 7.504.75 6.753-1.125 5.253-3.377 3.001-5.628-.375-6.378-3.002-7.503-7.503-8.63-7.504-6.002-7.503-4.127-7.504-3.002-8.254-1.875h-7.128l-5.253 2.25z"
+      ></path>
+    </svg>
+  );
+}
---
+++ packages/geoprocessing/src/components/diagram/WatersDiagram.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { WatersBackgroundSimple } from "./WatersBackgroundSimple";
+
+/** Type accepted by WatersDiagram to set translatable text labels  */
+interface Label {
+  key: string;
+  labelText?: string;
+  x?: string | number;
+  y?: string | number;
+  style?: React.CSSProperties;
+}
+
+/** Array of Labels to be inserted into the waters diagram */
+export interface LabelProps {
+  /** overrides default labels with same key id, otherwise adds as new label */
+  labels?: Label[];
+}
+
+/** Serves up a translatable SVG image showing nautical boundaries */
+export const WatersDiagram: React.FunctionComponent<LabelProps> = ({
+  labels,
+}) => {
+  // Allows translation of labels
+  const { t } = useTranslation();
+
+  // Default labels for land, shoreline, nearshore, offshore, and EEZ
+  const landLabel = t("Land");
+  const shorelineLabel = t("Shoreline");
+  const nearshoreLabel = t("Nearshore\n(0-12 nautical miles)");
+  const offshoreLabel = t("Offshore\n(12-200 nautical miles)");
+  const eezLabel = t("Exclusive Economic Zone\n(0-200 nautical miles)");
+
+  const labelsFinal: Label[] = [
+    { key: "land", labelText: landLabel, x: 220, y: 650 },
+    { key: "shoreline", labelText: shorelineLabel, x: 400, y: 580 },
+    {
+      key: "nearshore",
+      labelText: nearshoreLabel,
+      x: 160,
+      y: 480,
+    },
+    {
+      key: "offshore",
+      labelText: offshoreLabel,
+      x: 320,
+      y: 350,
+    },
+    {
+      key: "eez",
+      labelText: eezLabel,
+      x: 50,
+      y: 300,
+    },
+  ].map((label) => ({
+    //Adding default style
+    ...label,
+    style: { font: "12pt Helvetica, Arial, sans-serif", whiteSpace: "pre" },
+  }));
+
+  // If no user-generated labels, use defaults
+  labels?.forEach((label) => {
+    // Find matching label if exists
+    const foundIndex = labelsFinal.findIndex(
+      (curLabel) => label.key === curLabel.key
+    );
+
+    // If no matching label key, add label
+    if (foundIndex === -1) labelsFinal.push(label);
+    else {
+      // If matching label found, update it
+      labelsFinal[foundIndex] = {
+        ...labelsFinal[foundIndex],
+        ...label,
+        style: {
+          ...labelsFinal[foundIndex].style,
+          ...label.style,
+        },
+      };
+    }
+  });
+
+  return (
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 700">
+        <WatersBackgroundSimple />
+
+        {/* Generates labels based on labels param or defaults */}
+        {labelsFinal.map((label) => (
+          <text key={label.key} x={label.x} y={label.y} style={label.style}>
+            {label.labelText}
+          </text>
+        ))}
+      </svg>
+    </div>
+  );
+};

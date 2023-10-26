25d99f22bc45796f840fd417e4ba79808aa40e4c
Tim Welch <tim.j.welch@gmail.com>
2023-10-26 15:32:38 -0700

    template-ocean-eez: fix the property filter field name, and smoke test

--- packages/template-ocean-eez/src/functions/boundaryAreaOverlap.ts
+++ packages/template-ocean-eez/src/functions/boundaryAreaOverlap.ts
@@ -58,7 +58,7 @@ export async function boundaryAreaOverlap(
         const url = project.getDatasourceUrl(ds);
         const polys = await getFeatures(ds, url, {
           propertyFilter: {
-            property: "UNION",
+            property: "GEONAME",
             values: [project.basic.planningAreaId],
           },
           bbox: sketchBox,

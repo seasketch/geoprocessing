03bf730dbc5685fb766df790d370b1efb5b3c4e0
Tim Welch <tim.j.welch@gmail.com>
2024-03-12 09:29:21 -0700

    add vitest dep formally, add vitest-setup loading jest-dom

--- packages/geoprocessing/vitest.config.ts
+++ packages/geoprocessing/vitest.config.ts
@@ -1,13 +1,17 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     name: "src",
     root: "./src",
     // include: ["**/*.{test}.?(c|m)[jt]s?(x)"],
-    exclude: ["src/components/**"],
+    exclude: [...configDefaults.exclude, "./src/toolbox/**"],
     environment: "node",
     globals: true,
+    setupFiles: ["./vitest-setup.ts"],
   },
 });

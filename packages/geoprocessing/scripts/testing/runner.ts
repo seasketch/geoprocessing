66f9416bdb676643963936b9b7b4f5ac6673c78b
Tim Welch <tim.j.welch@gmail.com>
2024-04-02 09:01:30 -0700

    vitest comments

--- packages/geoprocessing/scripts/testing/runner.ts
+++ packages/geoprocessing/scripts/testing/runner.ts
@@ -5,18 +5,20 @@ import { configDefaults } from "vitest/config";
  * Manually parse user-provided CLI arguments from geoprocessing test command and run vitest
  */
 export default async function () {
-  // build vitest command from user-provided arguments after "--"
+  // build vitest command from user-provided arguments, then parse it
   const argv = ["vitest", ...process.argv.slice(2)];
   const { filter, options } = parseCLI(argv);
 
-  const defaultOptions = {
+  // Default to jsdom env, user can override to node environment in test files
+  const testOptions = {
     root: process.env.TEST_ROOT,
     exclude: [...configDefaults.exclude],
     environment: "jsdom",
     globals: true,
     run: true,
     passWithNoTests: true,
+    ...options, // merge user-provided options
   };
 
-  startVitest("test", filter, { ...defaultOptions, ...options });
+  startVitest("test", filter, testOptions);
 }

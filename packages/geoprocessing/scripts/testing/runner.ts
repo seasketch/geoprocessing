d64644f6fe476f0556374609b86953117716a24f
Tim Welch <tim.j.welch@gmail.com>
2024-04-02 08:40:04 -0700

    simplify test commands, tune test config

--- packages/geoprocessing/scripts/testing/runner.ts
+++ packages/geoprocessing/scripts/testing/runner.ts
@@ -6,19 +6,17 @@ import { configDefaults } from "vitest/config";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default async function () {
+  // build vitest command, adding user-provided arguments after "--"
   const argv = ["vitest", ...process.argv.slice(2)];
-  console.log("dirname", __dirname);
-  console.log("args", JSON.stringify(argv));
-  console.log("process.env", process.env.TEST_ROOT);
-
   const { filter, options } = parseCLI(argv);
 
   const defaultOptions = {
-    name: "node-tests",
     root: process.env.TEST_ROOT,
     exclude: [...configDefaults.exclude],
     environment: "jsdom",
     globals: true,
+    run: true,
+    passWithNoTests: true,
   };
 
   startVitest("test", filter, { ...defaultOptions, ...options });

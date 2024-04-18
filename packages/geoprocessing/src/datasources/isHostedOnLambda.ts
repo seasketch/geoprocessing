233223f0b78f40435ba2bd8cbcba2be3ceca1cc1
Tim Welch <tim.j.welch@gmail.com>
2024-04-18 13:48:16 -0700

    cleanup isHostedOnLambda

--- packages/geoprocessing/src/datasources/isHostedOnLambda.ts
+++ packages/geoprocessing/src/datasources/isHostedOnLambda.ts
@@ -1,5 +1,14 @@
-export default !!(
-  process &&
-  process.env &&
-  (process.env.LAMBDA_TASK_ROOT || process.env.AWS_EXECUTION_ENV)
-);
+// Check if code seems to be currently executing in Lambda environment
+const hasProcess = (() => {
+  if (!process) return false;
+  if (
+    process.env &&
+    (process.env.LAMBDA_TASK_ROOT || process.env.AWS_EXECUTION_ENV)
+  )
+    return true;
+  return false;
+})();
+if (!process) {
+}
+
+export default hasProcess;

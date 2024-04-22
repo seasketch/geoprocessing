cb7503a4eab9d0901ec3a9513cf64fb69b4e7298
Tim Welch <tim.j.welch@gmail.com>
2024-04-21 19:59:21 -0700

    simplify build client gp path discovery

--- packages/geoprocessing/scripts/build/build-client.sh
+++ packages/geoprocessing/scripts/build/build-client.sh
@@ -5,17 +5,18 @@ rm -rf .build-web
 mkdir .build-web
 cp -r src/i18n/lang .build-web
 cp -r src/i18n/baseLang .build-web
-# Determine correct path. Need to be in @seasketch/geoprocessing root
+
+# Determine path to @seasketch/geoprocessing root
+GP_PATH=GP_NOT_FOUND
 if test -f "../geoprocessing/scripts/build/build-client.sh"; then
   # in monorepo
-  # cd ../geoprocessing
-  ANALYZE=$ANALYZE NOMINIFY=$NOMINIFY npx tsx ../geoprocessing/scripts/build/buildClient.ts
-  cp ../geoprocessing/src/assets/favicon.ico $PROJECT_PATH/.build-web/
+  GP_PATH=../geoprocessing
 else
   # production reporting tool
-  # cd node_modules/@seasketch/geoprocessing
-  ANALYZE=$ANALYZE NOMINIFY=$NOMINIFY npx tsx node_modules/@seasketch/geoprocessing/scripts/build/buildClient.ts
-  cp node_modules/@seasketch/geoprocessing/src/assets/favicon.ico $PROJECT_PATH/.build-web/
+  GP_PATH=node_modules/@seasketch/geoprocessing
 fi
+
+ANALYZE=$ANALYZE NOMINIFY=$NOMINIFY npx tsx $GP_PATH/scripts/build/buildClient.ts
+cp $G_PATH/src/assets/favicon.ico $PROJECT_PATH/.build-web/
 
 echo ""

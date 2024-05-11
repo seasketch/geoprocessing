4bc60f7f183b27569aad1f6557a8e258ac552b38
Tim Welch <tim.j.welch@gmail.com>
2024-05-11 11:45:10 -0700

    remove vite worker config since web worker not being used

--- packages/geoprocessing/vite.config.ts
+++ packages/geoprocessing/vite.config.ts
@@ -5,9 +5,6 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  worker: {
-    format: "es",
-  },
   build: {
     sourcemap: true,
     minify: true,

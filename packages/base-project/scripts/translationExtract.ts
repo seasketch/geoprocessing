3e264759ad22a671aff687897a53e08564d63643
Tim Welch <tim.j.welch@gmail.com>
2024-05-22 08:58:26 -0700

    add debug script launcher bug: translation extract had lost command to save output. bug: prepare running translationInstall each time needs to run extract after to not lose project terms

--- packages/base-project/scripts/translationExtract.ts
+++ packages/base-project/scripts/translationExtract.ts
@@ -5,8 +5,9 @@ $.verbose = true;
 
 await $`rm -rf src/i18n/lang/en`;
 await $`NODE_ENV=development npx babel 'src/**/*.{ts,tsx}' > /dev/null`;
+await $`mv extractedTranslations/en src/i18n/lang/en`;
 await $`rm -rf extractedTranslations`;
 await $`npx tsx scripts/extractExtraTerms.ts`;
 console.log(
-  "Translations extracted to src/lang/en and src/i18n/extraTerms.json"
+  "Translations extracted to src/i18n/lang/en and src/i18n/extraTerms.json"
 );

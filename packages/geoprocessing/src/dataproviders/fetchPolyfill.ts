ac11f4843233925aa448946654750be94a80fa6b
Tim Welch <tim.j.welch@gmail.com>
2024-06-12 09:01:04 -0700

    🐛 socket hang up error (#300)
    * strictly set keepalive false in http agent, fix console.time label name collision
    
    * add infinity maxSockets
    
    * drop global maxSockets
    
    * pass options instead of init, use agent switcher depending on protocol
    
    * comment
    
    * make sure console time label consistent start and end

--- packages/geoprocessing/src/dataproviders/fetchPolyfill.ts
+++ packages/geoprocessing/src/dataproviders/fetchPolyfill.ts
@@ -1,20 +1,38 @@
 import fetch, {
-  Blob,
-  blobFrom,
-  blobFromSync,
-  File,
-  fileFrom,
-  fileFromSync,
-  FormData,
   Headers,
   Request,
   Response,
+  RequestInit,
+  RequestInfo,
 } from "node-fetch";
+import https from "https";
+import http from "http";
+
+// Custom agents that force connection to be closed after request complete (no reuse)
+// Overcomes Node 19+ change to set keepalive to true and reuse connections
+const httpAgent = new http.Agent({ keepAlive: false, maxSockets: Infinity });
+const httpsAgent = new https.Agent({ keepAlive: false, maxSockets: Infinity });
+const customFetch = async (
+  url: URL | RequestInfo,
+  init?: RequestInit
+): Promise<Response> => {
+  const options = {
+    ...init,
+    agent: function (_parsedURL) {
+      if (_parsedURL.protocol == "http:") {
+        return httpAgent;
+      } else {
+        return httpsAgent;
+      }
+    },
+  };
+  return fetch(url, options);
+};
 
 // Always polyfill, because of issue with native Node v20 fetch API causing request timeout.  Node v21 and up should be stable, and this polyfill can be removed in the future
 if (globalThis) {
   //@ts-ignore
-  globalThis.fetch = fetch;
+  globalThis.fetch = customFetch;
   //@ts-ignore
   globalThis.Headers = Headers;
   //@ts-ignore

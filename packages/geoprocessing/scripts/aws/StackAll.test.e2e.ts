b476445868875da5136226273031fc5da033975f
Tim Welch <tim.j.welch@gmail.com>
2024-07-26 10:36:52 -0700

    bug: task metricGroup results not saved when just one group (#319)
    * bug: support 1 metric group split
    
    * StackAll - increase timeout

--- packages/geoprocessing/scripts/aws/StackAll.test.e2e.ts
+++ packages/geoprocessing/scripts/aws/StackAll.test.e2e.ts
@@ -172,5 +172,5 @@ describe("GeoprocessingStack - all components", () => {
       Handler: getHandlerPointer(manifest.geoprocessingFunctions[1]),
       Runtime: config.NODE_RUNTIME.name,
     });
-  }, 30000);
+  }, 60000);
 });

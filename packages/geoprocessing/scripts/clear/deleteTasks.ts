e90a2f683cf0f7ba2aa428d619f982229d2b888b
Tim Welch <tim.j.welch@gmail.com>
2024-07-31 22:58:01 -0700

    deleteTasks: refactor to delete a batch as soon as its ready

--- packages/geoprocessing/scripts/clear/deleteTasks.ts
+++ packages/geoprocessing/scripts/clear/deleteTasks.ts
@@ -6,9 +6,10 @@ import {
   paginateScan,
   DynamoDBDocumentPaginationConfiguration,
 } from "@aws-sdk/lib-dynamodb";
-import { chunk } from "../../src/helpers/chunk.js";
 
-const MAX_BATCH_UPDATE = 25; // 25 is the maximum number of items that can be deleted in a single batch
+const MAX_BATCH_DELETE = 25; // 25 is the maximum number of items that can be deleted in a single batch
+
+type TaskKey = { id: string; service: string };
 
 /**
  * Clear all results from task table
@@ -38,7 +39,7 @@ export async function deleteTasks(
     pageSize: 25,
   };
 
-  const taskKeys: { id: string; service: string }[] = [];
+  const taskKeys: TaskKey[] = [];
 
   let query: QueryCommandInput = {
     TableName: tableName,
@@ -57,60 +58,77 @@ export async function deleteTasks(
 
   const pager = paginateScan(paginatorConfig, query);
 
+  const promises: Promise<void>[] = [];
+  let batchNum = 0;
   for await (const result of pager) {
     if (result && result.Items) {
-      result.Items.forEach((item) => {
+      result.Items.forEach(async (item, index, result) => {
         taskKeys.push({
           id: item.id,
           service: item.service,
         });
+
+        // Delete tasks in batches
+        if (taskKeys.length >= MAX_BATCH_DELETE) {
+          const taskKeyBatch = taskKeys.splice(0, MAX_BATCH_DELETE); // deletes items from taskKeys array
+          promises.push(
+            batchDeleteTasks(docClient, taskKeyBatch, tableName, batchNum)
+          );
+          batchNum += 1;
+        }
       });
     }
   }
 
-  // Convert task keys to chunks of delete requests
+  // Delete any remaining tasks
+  if (taskKeys.length > 0 && taskKeys.length < MAX_BATCH_DELETE) {
+    promises.push(batchDeleteTasks(docClient, taskKeys, tableName, batchNum));
+  }
 
-  const deleteRequestChunks: object[][] = chunk(taskKeys, MAX_BATCH_UPDATE).map(
-    (chunk) => {
-      return chunk.map((taskKey) => {
-        return {
-          DeleteRequest: {
-            Key: {
-              id: taskKey.id,
-              service: taskKey.service,
-            },
-          },
-        };
-      });
-    }
-  );
-
-  // create and execute batch command for each chunk of items to delete
-
-  if (deleteRequestChunks.length > 0) {
+  if (batchNum === 0) {
     console.log(
-      `Clearing all results in ${projectName}${serviceName ? " for function " + serviceName : ""}`
-    );
-  } else {
-    console.log(
-      `No results found in ${projectName}${serviceName ? " for function " + serviceName : ""}`
+      `No results found in DynamoDB table ${tableName} ${serviceName ? "for service " + serviceName : ""}`
     );
   }
 
-  deleteRequestChunks.forEach(async (deleteRequestChunk, index) => {
-    const deleteRequest = {
-      RequestItems: {
-        [tableName]: deleteRequestChunk,
+  await Promise.all(promises);
+}
+
+/**
+ * Batch delete tasks in single query
+ */
+async function batchDeleteTasks(
+  docClient: DynamoDBDocument,
+  taskKeys: TaskKey[],
+  tableName: string,
+  index: number
+) {
+  const deleteRequestChunk: object[] = taskKeys.map((taskKey) => {
+    return {
+      DeleteRequest: {
+        Key: {
+          id: taskKey.id,
+          service: taskKey.service,
+        },
       },
     };
-    const lowerBound = index === 0 ? 1 : index * MAX_BATCH_UPDATE;
-    const upperBound =
-      index * MAX_BATCH_UPDATE +
-      (deleteRequestChunk.length < MAX_BATCH_UPDATE
-        ? deleteRequestChunk.length
-        : MAX_BATCH_UPDATE);
-    console.log(`Deleting items ${lowerBound} - ${upperBound}`);
-    const deleteCommand = new BatchWriteCommand(deleteRequest);
-    await docClient.send(deleteCommand);
   });
+
+  const deleteRequest = {
+    RequestItems: {
+      [tableName]: deleteRequestChunk,
+    },
+  };
+  const lowerBound = index * MAX_BATCH_DELETE + 1;
+  // console.log("index", index);
+  // console.log("deleteRequestChunk.length", deleteRequestChunk.length);
+  // console.log("MAX_BATCH_DELETE", MAX_BATCH_DELETE);
+  const upperBound =
+    index * MAX_BATCH_DELETE +
+    (deleteRequestChunk.length < MAX_BATCH_DELETE
+      ? deleteRequestChunk.length
+      : MAX_BATCH_DELETE);
+  console.log(`Deleting items ${lowerBound} - ${upperBound}`);
+  const deleteCommand = new BatchWriteCommand(deleteRequest);
+  await docClient.send(deleteCommand);
 }

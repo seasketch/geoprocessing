c3e4b25cf9aa6474edcc42c60c702fb5d1a27aac
Tim Welch <tim.j.welch@gmail.com>
2024-09-10 16:10:49 -0700

    fix rtl

--- packages/geoprocessing/src/components/i18n/ReportTextDirection.tsx
+++ packages/geoprocessing/src/components/i18n/ReportTextDirection.tsx
@@ -6,7 +6,7 @@ export const ReportTextDirection: React.FunctionComponent<{
   children: ReactNode;
   style?: React.CSSProperties;
 }> = ({ style, children }) => {
-  const [isRtl] = useLanguage();
+  const [language, changeLanguage, isRtl] = useLanguage();
   return (
     <div style={style} dir={isRtl ? "rtl" : "ltr"}>
       {children}

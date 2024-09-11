e3eefd718030a94b900b8103a09a4c1c6f816062
Tim Welch <tim.j.welch@gmail.com>
2024-09-11 13:09:10 -0700

    update gp in dependencies not devDependencies

--- packages/geoprocessing/scripts/init/createProject.ts
+++ packages/geoprocessing/scripts/init/createProject.ts
@@ -133,17 +133,17 @@ export async function createProject(
   };
 
   if (gpVersion) {
-    if (packageJSON.devDependencies) {
-      packageJSON.devDependencies["@seasketch/geoprocessing"] = gpVersion;
+    if (packageJSON.dependencies) {
+      packageJSON.dependencies["@seasketch/geoprocessing"] = gpVersion;
     } else {
-      packageJSON.devDependencies = { "@seasketch/geoprocessing": gpVersion };
+      packageJSON.dependencies = { "@seasketch/geoprocessing": gpVersion };
     }
     spinner.succeed(`Installing user-defined GP version ${gpVersion}`);
   } else {
-    if (packageJSON.devDependencies) {
-      packageJSON.devDependencies["@seasketch/geoprocessing"] = curGpVersion;
+    if (packageJSON.dependencies) {
+      packageJSON.dependencies["@seasketch/geoprocessing"] = curGpVersion;
     } else {
-      packageJSON.devDependencies = {
+      packageJSON.dependencies = {
         "@seasketch/geoprocessing": curGpVersion,
       };
     }

ef1f5ac18a9a32f6c6bc0192d60dc25cba87b751
Tim Welch <tim.j.welch@gmail.com>
2024-09-20 09:11:33 -0700

    rename createStack to deployStack

--- packages/geoprocessing/scripts/deploy/bootstrap.sh
+++ packages/geoprocessing/scripts/deploy/bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PROJECT_PATH=$(pwd)
 cd node_modules/@seasketch/geoprocessing
-npx aws-cdk --app "node dist/scripts/deploy/createStack.js" bootstrap
+npx aws-cdk --app "node dist/scripts/deploy/deployStack.js" bootstrap
--- packages/geoprocessing/scripts/deploy/destroy.sh
+++ packages/geoprocessing/scripts/deploy/destroy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export PROJECT_PATH=$(pwd)
 cd node_modules/@seasketch/geoprocessing
-npx aws-cdk destroy --force --app "node dist/scripts/deploy/createStack.js"
+npx aws-cdk destroy --force --app "node dist/scripts/deploy/deployStack.js"
 

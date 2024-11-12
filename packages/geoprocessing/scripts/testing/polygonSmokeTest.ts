48adbbfa2794c6f05e90989ed7eee7a2a7e73849
Tim Welch <tim.j.welch@gmail.com>
2024-11-12 13:55:06 -0800

    clean up polygonSmokeTest doc

--- packages/geoprocessing/scripts/testing/polygonSmokeTest.ts
+++ packages/geoprocessing/scripts/testing/polygonSmokeTest.ts
@@ -4,8 +4,12 @@ import { Sketch, ValidationError } from "../../src/index.js";
 import { booleanValid } from "@turf/turf";
 
 /**
- * Runs smoke test for a preprocessor function taking polygon input
- * Runs preprocessor function for all example polygon sketches
+ * Runs a basic smoke test on a preprocessor function that takes feature or sketch polygon input
+ * For each example provided, runs the preprocessing function and checks that the output is truthy,
+ * and that the geometry is valid according to the OGC spec using turf.booleanValid
+ * then outputs the result to examples/output directory.  If you need more comprehensive tests,
+ * consider writing additional tests after calling this test or simply copying the source code for this test
+ * function to create your own.
  */
 export const polygonSmokeTest = (
   preprocessorFunc: (feature: Feature) => Promise<Feature>,
@@ -14,8 +18,9 @@ export const polygonSmokeTest = (
   /** Feature or Sketch examples to run against */
   examples: Feature[] | Sketch[],
   options: {
-    /** timeout for test run in milliseconds, defaults to 10000 */
+    /** timeout for test run in milliseconds, defaults to 10000 or 10 seconds */
     timeout?: number;
+    /** If true console outputs the name of the example on start of run */
     debug?: boolean;
   } = {},
 ) => {
@@ -30,7 +35,7 @@ export const polygonSmokeTest = (
       `${preprocessorName}Smoke`,
       async () => {
         if (examples.length === 0) {
-          console.log(`No example sketches provided`);
+          console.log(`No examples provided`);
         }
         for (const example of examples) {
           if (debug) {
